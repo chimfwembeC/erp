1d4d813aa66abf9670d733271df21d7f75f83116
chimfwembeC <chimfwembek@kamstar.tech>
2025-01-20 16:43:44 +0200

    added socials to settings seeder and appservice provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,9 @@ class AppServiceProvider extends ServiceProvider
             'logo' => function () {
                 return DB::table('settings')->where('key', 'branding_logo')->value('value');
             },
+            'socials' => function () {
+                return DB::table('settings')->whereIn('key', ['site_facebook_url', 'site_twitter_url', 'site_instagram_url', 'site_linkedin_url'])->value('value');
+            },
         ]);
     }
 }
